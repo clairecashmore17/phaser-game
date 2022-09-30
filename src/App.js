37faecf1f359f2e53dd57f682b552fdf13ff5cf0
Claire Cashmore <clairecashmore2017@gmail.com>
2022-09-30 10:38:16 -0400

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Phaser, { Game } from "phaser";
+import PhaserMatterCollisionPlugin from "phaser-matter-collision-plugin";
+import { useCallback, useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [game, setGame] = useState(null);
+  // Creating game inside a useEffect in order to ensure 1 instance is created
+  useEffect(() => {
+    console.log("Going into useEffect");
+    console.log(game);
+    if (game) {
+      console.log("game detected. stop creation");
+      return;
+    }
+    const phaserGame = new Phaser.Game({
+      width: 512,
+      height: 412,
+      backgroundColor: "#333333",
+      type: Phaser.AUTO,
+      parent: "survival-game",
+      scene: [],
+      scale: {
+        zoom: 2,
+      },
+      physics: {
+        default: "matter",
+        matter: {
+          debug: true,
+          gravity: { y: 0 },
+        },
+      },
+      plugins: {
+        scene: [
+          {
+            plugin: PhaserMatterCollisionPlugin,
+            key: "matterCollision",
+            mapping: "matterCollision",
+          },
+        ],
+      },
+    });
+
+    setGame(phaserGame);
+  }, [game]);
 }
 
 export default App;
