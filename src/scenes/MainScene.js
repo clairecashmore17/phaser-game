162866dac393ded5e8d8ff198d8639264bb69217
Claire Cashmore <clairecashmore2017@gmail.com>
2022-10-02 16:22:54 -0400

    map showing up

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -11,21 +11,22 @@ export default class MainScene extends Phaser.Scene {
   //this is where we load all our images
   preload() {
     Player.preload(this);
+    this.load.image("tiles", "../../assets/maps/IceTileset.png");
+    this.load.tilemapTiledJSON("map", "assets/maps/snow_map.json");
   }
   //create our game objects
   create() {
+    //map
+    const map = this.make.tilemap({ key: "map" });
+    const tileset = map.addTilesetImage("IceTileset", "tiles", 32, 32);
+    const layer1 = map.createLayer("Tile Layer 1", tileset, 0, 0);
+    const layer2 = map.createLayer("Tile Layer 2", tileset, 0, 0);
+
     //creating new physics sprite
     this.player = new Player({
       scene: this,
       x: 0,
       y: 0,
-      texture: "bear",
-      frame: "bear_idle_1",
-    });
-    let textPlayer = new Player({
-      scene: this,
-      x: 100,
-      y: 100,
       texture: "bear",
       frame: "bear_idle_1",
     });
